1f92815d48beab613b500fa4eea61b35dc42253e
chfuchte <christian0511@gmx.de>
2024-07-15 21:23:07 +0200

    init

---
+++ lua/chfuchte/init.lua
@@ -0,0 +1,4 @@
+require("chfuchte.set")
+require("chfuchte.remap")
+require("chfuchte.init_lazy")
+
---
+++ lua/chfuchte/lazy/colorscheme.lua
@@ -0,0 +1,28 @@
+function SetColors(color)
+    color = color or "tokyonight-night"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+end
+
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark",                   
+                    floats = "dark",               
+                },
+            })
+            SetColors()
+        end
+    },
+}
