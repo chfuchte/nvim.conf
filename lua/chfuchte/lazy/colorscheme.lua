b5b16eecc9c52586b4832cb15d8238378ab7a390
chfuchte <christian0511@gmx.de>
2024-07-20 12:17:26 +0200

    update theme

--- lua/chfuchte/lazy/colorscheme.lua
+++ lua/chfuchte/lazy/colorscheme.lua
@@ -1,9 +1,9 @@
 function SetColors(color)
-    color = color or "tokyonight"
+    color = color or "nordic"
     vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 end
 
 return {
@@ -13,16 +13,24 @@ return {
         priority = 1000,
         config = function()
             require("tokyonight").setup({
-                transparent = true,
+                transparent = false,
                 terminal_colors = true,
                 styles = {
                     comments = { italic = false },
                     keywords = { italic = false },
-                    sidebars = "dark",                   
-                    floats = "dark",               
+                    sidebars = "dark",
+                    floats = "dark",
                 },
             })
             SetColors()
         end
     },
+    {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('nordic').load()
+    end
+}
 }
